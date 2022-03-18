162494d95a5bc5b934c0fbbe4c840c33718ef35c
Tad Luedeke <tluedeke@brotherhoodmutual.com>
2022-03-18 16:04:01 -0400

    Added life tracking, while loop to end combat when one combatant is dead.

--- src/com/dungeonquest/Main.java
+++ src/com/dungeonquest/Main.java
@@ -7,64 +7,119 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        Boolean newRound = true;
+
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name, Hero.");
+        String heroName = scanner.nextLine();
 
-        System.out.println("Hero, choose your attack: S for 'slash', L for 'leap aside' or M for 'mighty blow'.");
+        System.out.println("How many Life Points do you have " + heroName + "?");
+        int heroLP = Integer.parseInt(scanner.nextLine());
 
-        String heroAttackString = "";
+        System.out.println("Enter the name of the monster you are facing.");
+        String monster = scanner.nextLine();
 
-        String heroAttack = scanner.nextLine().toUpperCase(Locale.ROOT);
-        switch (heroAttack) {
-            case "S":
-                heroAttackString = "Slash";
-                System.out.println("You chose to Slash!");
-                break;
-            case "L":
-                heroAttackString = "Leap Aside";
-                System.out.println("You chose to Leap Aside!");
-                break;
-            case "M":
-                heroAttackString = "Mighty Blow!";
-                System.out.println("You chose the Mighty Blow!!");
-                break;
-            default:
-                System.out.println("Please choose your attack: S for 'slash', L for 'leap aside' or M for 'mighty blow'.");
-        }
+        Random monsterLPRandom = new Random();
+        int monsterLP = monsterLPRandom.nextInt(8 - 1 + 1) + 1;
+        //For testing, want to see monster LP at the beginning.
+        //System.out.println("The " + monster + " has " + monsterLP + " Life Points.");
 
-        String[] monsterAttackOptions = {"Slash", "Leap Aside", "Mighty Blow!"};
+        System.out.println("Good luck " + heroName + ". You have " + heroLP + " Life Points.");
 
-        int monsterChoice = new Random().nextInt(monsterAttackOptions.length);
-        String monsterAttack = monsterAttackOptions[monsterChoice];
+        while (newRound) {
+            System.out.println(heroName + ", choose your attack: S for 'slash', L for 'leap aside' or M for 'mighty blow'.");
 
-        System.out.println("The monster is deciding on his attack...");
-        try {
-            Thread.sleep(4000);
-        } catch(InterruptedException exception) {
-            Thread.currentThread().interrupt();
-        }
+            String heroAttackString;
 
-        System.out.println("The monster chooses " + monsterAttack + "!");
+            String[] heroAttackOptions = {"Slash", "Leap Aside", "Mighty Blow!"};
 
-        if (heroAttackString.equals(monsterAttack)) {
-            System.out.println("You and your foe strike your blades together!");
-            System.out.println("You take 1 damage, but the monster also takes 1 damage.");
-        } else if (heroAttackString.equals("Mighty Blow!") && monsterAttack.equals("Slash")) {
-            System.out.println("You smite the monster! The monster takes 2 damage!");
-        } else if (heroAttackString.equals("Leap Aside") && monsterAttack.equals("Slash")) {
-            System.out.println("You try to leap out of the way, but the monster slashes you across the back!");
-            System.out.println("You take 1 damage.");
-        } else if (heroAttackString.equals("Slash") && monsterAttack.equals("Mighty Blow!")) {
-            System.out.println("You raise your sword, but the monster smites your blade aside!");
-            System.out.println("You take 1 damage.");
-        } else if (heroAttackString.equals("Slash") && monsterAttack.equals("Leap Aside")) {
-            System.out.println("The monster tries to leap out of the way, but you slash him across the back!");
-            System.out.println("The monster takes 1 damage.");
-        } else if (heroAttackString.equals("Mighty Blow!") && monsterAttack.equals("Leap Aside")) {
-            System.out.println("You swing with all your might, but the monster leaps aside!");
-            System.out.println("You take 1 damage.");
-        } else if (heroAttackString.equals("Leap Aside") && monsterAttack.equals("Mighty Blow!")) {
-            System.out.println("The monster swings mightily at you, but you leap under the blow!");
-            System.out.println("The monster takes 1 damage.");
+            String heroAttack = scanner.nextLine().toUpperCase(Locale.ROOT);
+            switch (heroAttack) {
+                case "S":
+                    heroAttackString = "Slash";
+                    System.out.println("You chose to Slash!");
+                    break;
+                case "L":
+                    heroAttackString = "Leap Aside";
+                    System.out.println("You chose to Leap Aside!");
+                    break;
+                case "M":
+                    heroAttackString = "Mighty Blow!";
+                    System.out.println("You chose the Mighty Blow!!");
+                    break;
+                default:
+                    int heroChoice = new Random().nextInt(heroAttackOptions.length);
+                    heroAttackString = heroAttackOptions[heroChoice];
+                    System.out.println("You have been randomly assigned " + heroAttackString + "!");
+            }
+
+            String[] monsterAttackOptions = {"Slash", "Leap Aside", "Mighty Blow!"};
+
+            int monsterChoice = new Random().nextInt(monsterAttackOptions.length);
+            String monsterAttack = monsterAttackOptions[monsterChoice];
+
+            System.out.println("The " + monster + " is deciding on his attack...");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException exception) {
+                Thread.currentThread().interrupt();
+            }
+
+            System.out.println("The " + monster + " chooses " + monsterAttack + "!");
+            System.out.println("--------------------------------------------------------");
+
+            if (heroAttackString.equals(monsterAttack)) {
+                System.out.println("You and the " + monster + " strike your blades together!");
+                System.out.println("You take 1 damage, but the " + monster + " also takes 1 damage.");
+                System.out.println("--------------------------------------------------------");
+                heroLP--;
+                monsterLP--;
+            } else if (heroAttackString.equals("Mighty Blow!") && monsterAttack.equals("Slash")) {
+                System.out.println("You smite the " + monster + "! The " + monster + " takes 2 damage!");
+                System.out.println("--------------------------------------------------------");
+                monsterLP = monsterLP - 2;
+            } else if (heroAttackString.equals("Leap Aside") && monsterAttack.equals("Slash")) {
+                System.out.println("You try to leap out of the way, but the " + monster + " slashes you across the back!");
+                System.out.println("You take 1 damage.");
+                System.out.println("--------------------------------------------------------");
+                heroLP--;
+            } else if (heroAttackString.equals("Slash") && monsterAttack.equals("Mighty Blow!")) {
+                System.out.println("You raise your sword, but the " + monster + " smites your blade aside!");
+                System.out.println("You take 1 damage.");
+                System.out.println("--------------------------------------------------------");
+                heroLP--;
+            } else if (heroAttackString.equals("Slash") && monsterAttack.equals("Leap Aside")) {
+                System.out.println("The " + monster + " tries to leap out of the way, but you slash him across the back!");
+                System.out.println("The " + monster + " takes 1 damage.");
+                System.out.println("--------------------------------------------------------");
+                monsterLP--;
+            } else if (heroAttackString.equals("Mighty Blow!") && monsterAttack.equals("Leap Aside")) {
+                System.out.println("You swing with all your might, but the " + monster + " leaps aside!");
+                System.out.println("You take 1 damage.");
+                System.out.println("--------------------------------------------------------");
+                heroLP--;
+            } else if (heroAttackString.equals("Leap Aside") && monsterAttack.equals("Mighty Blow!")) {
+                System.out.println("The " + monster + " swings mightily at you, but you leap under the blow!");
+                System.out.println("The " + monster + " takes 1 damage.");
+                System.out.println("--------------------------------------------------------");
+                monsterLP--;
+            }
+            if(monsterLP <= 0) {
+                System.out.println("You cleave the " + monster + " in half! You live to fight another day.");
+                newRound = false;
+            } else if(heroLP == 0) {
+                System.out.println("The " + monster + " buries his blade into your guts.");
+                System.out.println("You are dead.");
+                newRound = false;
+            } else {
+                System.out.println("You and the " + monster + " eye each other over your crossed blades. You have " + heroLP + " LP's remaining.");
+                //For testing purposes
+                //System.out.println("The " + monster + " has " + monsterLP + " LP's remaining.");
+                System.out.println("A new round begins!");
+                System.out.println("--------------------------------------------------------");
+                System.out.println(" ");
+            }
         }
     }
 }
